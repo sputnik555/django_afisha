dcb9ae02024a6f8bdd27d03e3356e111162d040a
Alexey Beletsky <sputnik2009@gmail.com>
2022-08-26 21:11:58 +0300

    Add Place model

--- where_to_go/places/models.py
+++ where_to_go/places/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
 # Create your models here.
+class Place(models.Model):
+    title = models.CharField(max_length=200)
+    description_short = models.TextField()
+    description_long = models.TextField()
+    latitude = models.DecimalField(max_digits=16, decimal_places=14)
+    longitude = models.DecimalField(max_digits=17, decimal_places=14)
+
+    def __str__(self):
+        return self.title
