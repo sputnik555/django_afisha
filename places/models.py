a9382fbd20f4fc1d763e5bec9f2172f3e6d1dfab
Alexey Beletsky <sputnik2009@gmail.com>
2022-09-05 23:53:14 +0300

    change blank\null parameters in models

--- places/models.py
+++ places/models.py
@@ -4,8 +4,8 @@ from tinymce.models import HTMLField
 
 class Place(models.Model):
     title = models.CharField(verbose_name='Заголовок', max_length=200)
-    description_short = models.TextField(verbose_name='Описание краткое')
-    description_long = HTMLField(verbose_name='Описание полное')
+    description_short = models.TextField(verbose_name='Описание краткое', blank=True)
+    description_long = HTMLField(verbose_name='Описание полное', blank=True)
     latitude = models.DecimalField(verbose_name='Широта', max_digits=16, decimal_places=14)
     longitude = models.DecimalField(verbose_name='Долгота', max_digits=17, decimal_places=14)
 
@@ -15,12 +15,17 @@ class Place(models.Model):
 
 class Image(models.Model):
     image = models.ImageField(verbose_name='Картинка')
-    place = models.ForeignKey(Place, models.SET_NULL, verbose_name='Место', null=True, related_name='images')
+    place = models.ForeignKey(
+        Place,
+        models.SET_NULL,
+        verbose_name='Место',
+        null=True,
+        blank=True,
+        related_name='images'
+    )
     order = models.PositiveIntegerField(
         verbose_name='Порядок',
-        default=0,
-        blank=False,
-        null=False,
+        default=0
     )
 
     class Meta:
