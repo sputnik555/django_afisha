3a1d06dacc4e9e2baf2f68866bbb590d255baa86
Alexey Beletsky <sputnik2009@gmail.com>
2022-09-11 12:01:51 +0300

    remove download demo data from devman repository

--- places/management/commands/load_place.py
+++ places/management/commands/load_place.py
@@ -1,13 +1,10 @@
-import urllib.parse
 from os.path import basename
 
 import requests
-from bs4 import BeautifulSoup
 from django.core.files.base import ContentFile
 from django.core.management.base import BaseCommand
-from places.models import Place, Image
 
-URL = 'https://github.com/devmanorg/where-to-go-places/tree/master/places'
+from places.models import Place, Image
 
 
 def add_place_in_db(place_data):
@@ -37,28 +34,6 @@ def add_place_in_db(place_data):
         )
 
 
-def get_place_urls():
-    response = requests.get(URL)
-    response.raise_for_status()
-    soup = get_soap(URL)
-    a_tags = soup.select("div.py-2 a.js-navigation-open")
-    place_urls = [urllib.parse.urljoin(URL, a_tag['href']) for a_tag in a_tags]
-    return place_urls
-
-
-def get_place_raw_url(place_url):
-    soup = get_soap(place_url)
-    a_tag_raw_url = soup.select_one('a#raw-url')
-    place_raw_url = urllib.parse.urljoin(place_url, a_tag_raw_url['href'])
-    return place_raw_url
-
-
-def get_soap(url):
-    response = requests.get(url)
-    response.raise_for_status()
-    return BeautifulSoup(response.text, 'lxml')
-
-
 class Command(BaseCommand):
     help = 'Скрипт загрузки данных в JSON-формате в базу данных'
 
@@ -66,13 +41,6 @@ class Command(BaseCommand):
         parser.add_argument('url', nargs='?', help='URL *.json файла для загрузки')
 
     def handle(self, *args, **options):
-        if options['url']:
-            response = requests.get(options['url'])
-            response.raise_for_status()
-            add_place_in_db(response.json())
-            return
-
-        for place_url in get_place_urls():
-            response = requests.get(get_place_raw_url(place_url))
-            response.raise_for_status()
-            add_place_in_db(response.json())
+        response = requests.get(options['url'])
+        response.raise_for_status()
+        add_place_in_db(response.json())
